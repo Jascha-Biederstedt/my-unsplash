9cbeacbf4c143f8c66752d8def350d314d0c6634
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-09-09 15:42:57 +0200

    Add search icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,11 +10,30 @@ const Navbar: React.FC<NavbarProps> = ({}) => {
         className='mr-3 h-6 md:h-9'
         alt='My Unsplash Logo'
       />
-      <input
-        type='search'
-        className='px-5 py-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
-        placeholder='Search by name'
-      />
+      <div className='relative'>
+        <div className='hidden md:flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+          <svg
+            aria-hidden='true'
+            className='w-5 h-5 text-gray-500'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              stroke-linecap='round'
+              stroke-linejoin='round'
+              stroke-width='2'
+              d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+            ></path>
+          </svg>
+        </div>
+        <input
+          type='search'
+          className='p-3 md:pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
+          placeholder='Search by name'
+        />
+      </div>
     </nav>
   );
 };
