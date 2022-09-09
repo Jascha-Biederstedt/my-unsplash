09f09155f05c61c34bff70ba64a858f425ecfdaa
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-09-09 14:50:03 +0200

    Add navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 
+import Navbar from 'components/Navbar';
 import Gallery from 'components/Gallery';
 
 const Home: NextPage = () => {
   return (
     <>
+      <Navbar />
       <Gallery />
     </>
   );
