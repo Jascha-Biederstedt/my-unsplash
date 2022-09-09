f90ae3127a71fad34df2c1b14a446133aa28d674
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-09-09 15:20:48 +0200

    Adjust navbar padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ interface NavbarProps {}
 
 const Navbar: React.FC<NavbarProps> = ({}) => {
   return (
-    <nav className='flex items-center container px-5 py-5 mx-auto lg:px-32'>
+    <nav className='flex items-center container px-5 py-6 mx-auto md:py-10 lg:px-32'>
       <img
         src='/my_unsplash_logo.svg'
         className='mr-3 h-6 md:h-9'
@@ -12,7 +12,7 @@ const Navbar: React.FC<NavbarProps> = ({}) => {
       />
       <input
         type='search'
-        className='px-4 py-2 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
+        className='px-5 py-3 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500'
         placeholder='Search by name'
       />
     </nav>
